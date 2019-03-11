2083a2e90a23e7729fb2203ccd1c53e74dfdc3cf
Anhcuongsuper <43609383+Anhcuongsuper@users.noreply.github.com>
2019-03-12 00:31:14 +0700

    jamether

---
+++ J4/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using J4.Models;
+
+namespace J4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<J4.Models.Recipes> Recipes { get; set; }
+        public DbSet<J4.Models.Contests> Contests { get; set; }
+    }
+}
